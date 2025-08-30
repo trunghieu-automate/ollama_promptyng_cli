c59e7421409bdc67eaa2f58a8d4f00a9668e9899
Trung Hieu <89647756+trunghieu-automate@users.noreply.github.com>
2025-08-30 20:37:06 +0700

    Update promptyng_cli.py comment _verify_model() method

--- promptyng_cli.py
+++ promptyng_cli.py
@@ -44,7 +44,7 @@ class OllamaInterface:
     
     def __init__(self, model_name: str = DEFAULT_MODEL):
         self.model_name = model_name
-        self._verify_model()
+        #self._verify_model()
     
     def _verify_model(self):
         """Check if the model is available in Ollama"""
